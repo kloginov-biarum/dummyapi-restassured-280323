15e814e0ce7ec0b6257768847bfb518fd4edd539
kloginov-biarum <kloginov@biarum.com>
2023-11-08 10:12:22 +0100

    add

--- src/test/java/tests/DeleteUserTest.java
+++ src/test/java/tests/DeleteUserTest.java
@@ -29,5 +29,30 @@ public class DeleteUserTest extends BaseTest{
 
 
     //delete deleted user
+    @Test
+    public void deleteDeletedUser(){
+    //Pre-steps:
+        UserFull requestBody = UserFull.builder()
+                .firstName("John")
+                .lastName("Black")
+                .email(getRandomEmail())
+                .gender("male")
+                .title("mr")
+                .build();
+        UserFull userFromCreateResponse = postRequest("/user/create", 200, requestBody)
+                .body().jsonPath().getObject("", UserFull.class);
+        deleteRequest("/user/" + userFromCreateResponse.getId(), 200);
+
+        String  errorMessage  =
+                deleteRequest("/user/" + userFromCreateResponse.getId(), 404).body().jsonPath().get("error");
+        assertEquals("RESOURCE_NOT_FOUND", errorMessage);
+    }
+
     //delete invalid user
+    @Test
+    public void deleteInvalidUser(){
+        String  errorMessage  =
+                deleteRequest("/user/" + "2345678987654323456780865678", 400).body().jsonPath().get("error");
+        assertEquals("PARAMS_NOT_VALID", errorMessage);
+    }
 }
